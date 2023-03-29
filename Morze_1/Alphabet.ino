a0ea33a1eb7ab0b37448600e4fdfb670709d7de6
Taras <97754097+OneReyzek@users.noreply.github.com>
2023-03-29 14:59:58 +0500

    обновил скетч
    добавил весь русский алфавит во вкладке

---
+++ Morze_1/Alphabet.ino
@@ -0,0 +1,39 @@
+/* 
+fDOT(); fDASH(); fPAUSE(); // А
+fDASH(); fDOT(); fDOT(); fDOT(); fPAUSE();// Б
+fDOT(); fDASH(); fDASH();  fPAUSE(); // В
+fDASH(); fDASH(); fDOT(); fPAUSE(); // Г
+fDASH(); fDOT(); fDOT(); fPAUSE();// Д
+fDOT(); fPAUSE();// Е/Ё
+fDOT(); fDOT(); fDOT(); fDASH(); fPAUSE();// Ж
+fDASH(); fDASH(); fDOT(); fDOT(); fPAUSE();//З
+fDOT(); fDOT(); fPAUSE();// И
+fDOT(); fDASH(); fDASH(); fDASH(); fPAUSE();// Й
+fDASH(); fDOT(); fDASH(); fPAUSE();// К
+fDOT(); fDASH(); fDOT(); fDOT(); fPAUSE();// Л
+fDASH(); fDASH(); fPAUSE();// М
+fDASH(); fDOT(); fPAUSE();// Н
+fDASH(); fDASH(); fDASH(); fPAUSE();// О
+fDOT(); fDASH(); fDASH(); fDOT(); fPAUSE();// П
+fDOT(); fDASH(); fDOT(); fPAUSE();// Р
+fDOT(); fDOT(); fDOT(); fPAUSE();// С
+fDASH(); fPAUSE();// Т
+fDOT(); fDOT(); fDASH(); fPAUSE();// У
+fDOT(); fDOT(); fDASH(); fDOT(); fPAUSE();// Ф
+fDOT(); fDOT(); fDOT(); fDOT(); fPAUSE();// Х
+fDASH(); fDOT(); fDASH(); fDOT(); fPAUSE();// Ц
+fDASH(); fDASH(); fDOT(); fPAUSE();// Ч
+fDASH(); fDASH(); fDASH(); fDASH(); fPAUSE();// Ш
+fDASH(); fDASH(); fDOT(); fDASH(); fPAUSE();// Щ
+fDOT(); fDASH(); fDASH(); fDOT(); fDASH(); fDOT(); fPAUSE();//Ъ
+fDASH(); fDOT(); fDASH(); fDASH(); fPAUSE();// Ы
+fDASH(); fDOT(); fDOT(); fDASH(); fPAUSE();// Ь
+fDOT(); fDOT(); fDASH(); fDOT(); fDOT(); fPAUSE(); // Э
+fDOT(); fDOT(); fDASH(); fDASH(); fPAUSE(); // Ю
+fDOT(); fDASH(); fDOT(); fDASH(); fPAUSE(); // Я
+
+
+fDOT();
+fDASH();
+fPAUSE();
+*/
